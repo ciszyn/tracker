37e85936720003c1d6b14a9344bfd45b274beee1
ciszyn <michal.ciszyn@gmail.com>
2023-02-14 11:56:57 +0100

    hiding privite keys

---
+++ src/config.index.ts
@@ -0,0 +1,20 @@
+import {writeFileSync} from 'fs';
+
+const targetPath = './src/environments/environment.prod.ts';
+
+const envConfigFile = `export const environment = {
+  firebase: {
+    apiKey: '${process.env['FIREBASE_API_KEY']}',
+    authDomain: 'pong-b7547.firebaseapp.com',
+    databaseURL:
+      'https://pong-b7547-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'pong-b7547',
+    storageBucket: 'pong-b7547.appspot.com',
+    messagingSenderId: '${process.env['FIREBASE_SENDER_ID']}',
+    appId: '${process.env['FIREBASE_APP_ID']}',
+  }
+  production: true,
+};
+`;
+
+writeFileSync(targetPath, envConfigFile, 'utf8');
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,11 +1,13 @@
+import { secrets } from "src/secrets/secrets";
+
 export const environment = {
   firebase: {
-    apiKey: 'AIzaSyB73EfNDslrlOa1iKogl1vQ_HsEi8NinOc ',
+    apiKey: secrets.firebaseApiKey,
     authDomain: 'tracker-f56ce.firebaseapp.com',
     databaseURL: 'https://tracker-f56ce-default-rtdb.europe-west1.firebasedatabase.app/',
     projectId: 'tracker-f56ce',
     storageBucket: 'tracker-f56ce.appspot.com',
-    messagingSenderId: '',
-    appId: '1:305225982657:web:26814ed343b0a1bc592a40',
+    messagingSenderId: secrets.firebaseMessagingSenderId,
+    appId: secrets.firebaseAppId,
   }
 }
